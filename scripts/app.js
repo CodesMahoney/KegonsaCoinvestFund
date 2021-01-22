55e6969d2caa2d24873a30886810fe1a7bf75c99
Dylan Hayes <DHayes@uwhealth.org>
2021-01-21 20:06:12 -0600

    fixed app.js locationprovider

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("app", ["ngRoute"]);
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when("/", {
             templateUrl: "main.html"
